a0f59e3c7439eb6026e84e6feb3332248b87492b
khaharr <khalidjj78200@icloud.com>
2025-10-11 21:54:54 +0200

    ajout tailwind css

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///home/kharrari01/Documents/Sadaqa-Jariya/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">✅ Tailwind fonctionne !</h1>
+  </div>
+</template>
